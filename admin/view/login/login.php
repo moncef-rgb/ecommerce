88db44bfa6b0c3f5f6948db0e4a82d6d1b192a29
nsoufa <moncef.boukhris@enis.tn>
2022-01-28 20:31:53 +0100

    nsoufa_logo

--- admin/view/login/login.php
+++ admin/view/login/login.php
@@ -26,9 +26,9 @@
           <div class="col-lg-4 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
               <div class="brand-logo">
-                <img src="../../view/assets/images/logo.png" alt="logo">
+                <img src="../../view/assets/images/logo11.png" alt="logo">
               </div>
-              <h4>Welcome to DAILYSHOP admin space</h4>
+              <h4>Welcome to InfoShop admin space</h4>
               <h6 class="font-weight-light" style="color:red"><?php 
               session_start();
               if (isset($_SESSION['error'])){
