1563c3d045465c7671b33d16b390bbb32bf7da7c
nsoufa <moncef.boukhris@enis.tn>
2022-01-28 21:22:34 +0100

    footer_nsoufa

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -53,7 +53,7 @@
         <div class="row">
           <div class="col-md-12">
             <div class="aa-footer-bottom-area">
-              <p>Designed by <a href="http://www.markups.io/">Firas Fendri / Zeinab ben abdalah / Eya Kteta / Emna Belhadj</a></p>
+              <p>Designed by <a href="http://www.markups.io/">Moncef Boukhris / Sourour Siala / Khouloud Yaich</a></p>
               <div class="aa-footer-payment"> <span class="fa fa-cc-mastercard"></span> <span class="fa fa-cc-visa"></span> <span class="fa fa-paypal"></span> <span class="fa fa-cc-discover"></span> </div>
             </div>
           </div>
